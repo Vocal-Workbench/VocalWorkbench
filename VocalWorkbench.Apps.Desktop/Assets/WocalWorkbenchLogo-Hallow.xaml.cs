5ce18d8895e87a2f6662c871f883b040a1a079b8
Éliette Priaulx <eliette@priaulx.co>
2019-12-09 00:18:44 +1100

    Add application icon

---
+++ VocalWorkbench.Apps.Desktop/Assets/WocalWorkbenchLogo-Hallow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VocalWorkbench.Apps.Desktop.Assets
+{
+    /// <summary>
+    /// Interaction logic for WocalWorkbenchLogo_Hallow.xaml
+    /// </summary>
+    public partial class WocalWorkbenchLogo_Hallow : UserControl
+    {
+        public WocalWorkbenchLogo_Hallow()
+        {
+            InitializeComponent();
+        }
+    }
+}
