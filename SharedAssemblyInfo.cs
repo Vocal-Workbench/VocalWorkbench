88684333f6fcd30123fb029730591bcd440d5c40
Éliette Priaulx <eliette@priaulx.co>
2019-12-09 09:24:10 +1100

    Add commen versioning and assembly info

---
+++ SharedAssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+
+[assembly: AssemblyDescription("Vocal Workbench")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("eliette.priaulx.co")]
+[assembly: AssemblyProduct("Vocal Workbench")]
+[assembly: AssemblyCopyright("Copyright �  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
