62e00c35c0090414b0914908aae285ee89e80a63
Éliette Priaulx <eliette@priaulx.co>
2019-12-09 13:14:28 +1100

    Implement Window title bar

---
+++ VocalWorkbench.Apps.Desktop/Controls/VocalWorkbenchLogo-Hallow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace VocalWorkbench.Apps.Desktop.Controls
+{
+    public partial class VocalWorkbenchLogo_Hallow : UserControl
+    {
+        public VocalWorkbenchLogo_Hallow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ VocalWorkbench.Apps.Desktop/Controls/VocalWorkbenchLogo-Solid.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace VocalWorkbench.Apps.Desktop.Controls
+{
+    public partial class VocalWorkbenchLogo_Solid : UserControl
+    {
+        public VocalWorkbenchLogo_Solid()
+        {
+            InitializeComponent();
+        }
+    }
+}
